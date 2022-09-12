0ecbb17565efdf0eb18bfd550b6ac1cc3378c154
Patrice <yemmene@gmail.com>
2022-09-11 21:10:53 -0500

    Few lines

--- Student-Management-System-1/src/main/java/Java/Learning/StudentManagementSystem1Application.java
+++ Student-Management-System-1/src/main/java/Java/Learning/StudentManagementSystem1Application.java
@@ -1,25 +1,12 @@
 package Java.Learning;
-
-
-
-
-
-
-
-
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
 @SpringBootApplication
 public class StudentManagementSystem1Application {
-	
-	
-	
-	
-	
-
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystem1Application.class, args);
 		
